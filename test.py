06ff87a510f4999244174adbc79eceb9826cd127
Joe Carey <joecarey001@gmail.com>
2017-03-03 18:35:18 -0600

    Clean up setup.py, include manifest and test file

---
+++ test.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+from pylawson.client import ms_samlpr, sec_api
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+ios = ms_samlpr.SamlSession(json_file='c:/users/sacarey/personal/google drive/dev/pylawson.json')
+
+print('bool:', ios.__bool__())
+
+print('is authenticated:', ios.is_authenticated)
+
+print('profile:', ios.profile)
+
+ios.close()
+
+ios = sec_api.SecApiSession(json_file='c:/users/sacarey/personal/google drive/dev/pylawson.json')
+
+print('bool:', ios.__bool__())
+
+print('is authenticated:', ios.is_authenticated)
+
+print('profile:', ios.profile)
+
+ios.close()
